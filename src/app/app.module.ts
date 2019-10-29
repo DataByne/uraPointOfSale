5ca4bfdf363b4ba3d5e08fb12426176a054c8e46
Hoang Le <38596968+peterle08@users.noreply.github.com>
2019-10-28 22:04:38 -0400

    update employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
   { path: '', redirectTo:'landing',pathMatch:'full'},
   { path:'**', component:LandingPageComponent}
 ];
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
