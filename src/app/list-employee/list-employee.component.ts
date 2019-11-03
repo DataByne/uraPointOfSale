7fd666ea06b808a1d75b3e5378642f3a110be2ca
Hoang Le <38596968+peterle08@users.noreply.github.com>
2019-11-03 08:03:49 -0500

    add table of employees to list-employee page, update insert-employee page, add update form (without queryParams), create employee.ts file

--- src/app/list-employee/list-employee.component.ts
+++ src/app/list-employee/list-employee.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import {FormsModule} from '@angular/forms';             // for creating form
-
+import {Employee} from './employee'
 @Component({
   selector: 'app-list-employee',
   templateUrl: './list-employee.component.html',
   styleUrls: ['./list-employee.component.css']
 })
 export class ListEmployeeComponent implements OnInit {
+  employees: Employee[] = [
+    {
+      employeeID: null,
+      firstName: "string",
+      lastName: "string",
+      middleName: "string",
+      ssn: 123456789,
+      gender: "string",
+      birthday: new Date(),
+      email: "string@gmail.com",
+      phoneNumber: 1,
+      address: "string",
+      hiringDate: new Date(),
+      salary: 100000,
+      position: "string",
+    }
 
+  ]
   constructor() { }
 
   ngOnInit() {
