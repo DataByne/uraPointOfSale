eceb89b3117d458c749710517601f292f041141a
Kyle Pugh <kpugh1@kent.edu>
2019-11-08 16:55:13 -0500

    Add bootstrap javascript CDN and fix login and registration forms

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Email or Username:', validators=[validators.InputRequired(), validators.DataRequired()])
+    password = PasswordField('Password:', validators=[validators.InputRequired(), validators.DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    email = StringField('Email:', validators=[validators.InputRequired(), validators.DataRequired()])
+    username = StringField('Username:', validators=[validators.InputRequired(), validators.DataRequired()])
+    password = PasswordField('Password:', validators=[validators.InputRequired(), validators.DataRequired()])
+    password_confirm = PasswordField('Confirm Password:', validators=[validators.InputRequired(), validators.DataRequired()])
     submit = SubmitField('Register')
 
