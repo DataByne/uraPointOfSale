73a39cd016186270c2609a9d42afdf1ca0356313
Kyle Pugh <kpugh1@kent.edu>
2019-11-08 14:20:40 -0500

    Add images directory and routing. Add footer links to footer instead of navigation bar.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, send_from_directory
 from app.forms import LoginForm
 
 @app.route( '/' )
 @app.route( '/index' )
 def index():
     return render_template('index.html', title='Home')
+    
+@app.route('/images/<path:path>')
+def send_js(path):
+    return send_from_directory('images', path)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
