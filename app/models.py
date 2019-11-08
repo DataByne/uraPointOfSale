1d72f6693f3d68d8831fb72dac048c2f7f52aa07
Ryan Yoak <ryan.yoak@gmail.com>
2019-11-08 15:54:50 -0500

    Has user model and compiles

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(150), index = True, unique = True)
+    creation_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
