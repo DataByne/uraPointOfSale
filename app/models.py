19e25e8ca6bb71f422c1e940c674d10d8957aa2d
Ryan Yoak <ryan.yoak@gmail.com>
2019-11-08 17:01:36 -0500

    one can now log in
    you can log in with
    Username: Ryan
    Password: 12345

--- app/models.py
+++ app/models.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(150), index = True, unique = True)
@@ -10,6 +17,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key = True)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, flash, redirect, url_for, request
 from app.forms import LoginForm, RegisterForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route( '/' )
 @app.route( '/index' )
@@ -21,5 +23,18 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title="Login", form=LoginForm())
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Login", form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
