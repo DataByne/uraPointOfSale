5e7f3a8cd958f738c422d5bb38a9fa7531bd5223
Ryan Yoak <ryan.yoak@gmail.com>
2019-11-08 01:32:37 -0500

    Most basic requirements of the website
    There is a landing page, link bar, and page for future login

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route( '/' )
 @app.route( '/index' )
 def index():
-    return "Welcome to Note Weave!"
+    return render_template('index.html', title='Home')
 
-
+@app.route('/login')
+def login():
+    return render_template('login.html', title="Login")
