b814e73d8c38b2587ddbd5fd09f65575cded5992
Kyle Pugh <kpugh1@kent.edu>
2019-11-29 21:10:02 -0500

    Change the login manager refresh message.

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.refresh_view = 'login'
-login_manager.needs_refresh_message = "Do something";
+login_manager.needs_refresh_message = "Session expires, please login again";
 login_manager.needs_refresh_message_category = "info"
 if app.config['MAIL_USERNAME'] is None or app.config['MAIL_PASSWORD'] is None:
     raise ServiceUnavailable('Mail service is not properly configured')
