8edb42616885cc30250f495be4742910c44b89e5
Kyle Pugh <kpugh1@kent.edu>
2019-11-29 23:00:16 -0500

    Allow usage of flask request context for behave testing.

--- features/environment.py
+++ features/environment.py
@@ -7,62 +7,62 @@ from app import app
 
 @fixture
 def splinter_browser( context, type='chrome' ):
-	"""Sets up a splinter browser session
+	  """Sets up a splinter browser session
 
-	Generator function that provides a web driver browser object for use in
-	testing. The browser defaults to a Chrome web driver.
+	  Generator function that provides a web driver browser object for use in
+	  testing. The browser defaults to a Chrome web driver.
 
-	Args:
-		context:	Behave testing context
-		type:		Specific webdriver to run
+	  Args:
+		    context:	Behave testing context
+		    type:		Specific webdriver to run
 
-	Yields:
-		A browser object attribute to the Behave testing module's context. The
-		type of the browser is set to whichever the generator is provided.
-	"""
-	context.browser = Browser( driver_name='flask', app=app )
-	yield context.browser
-	context.browser.quit()
-
+	  Yields:
+		    A browser object attribute to the Behave testing module's context. The
+		    type of the browser is set to whichever the generator is provided.
+	  """
+	  context.browser = Browser( driver_name='flask', app=app )
+	  yield context.browser
+	  context.browser.quit()
 
 @fixture
-def flask_client( context, *args, **kwargs ):
-	"""Sets up temporary flask context
+def flask_client(context, *args, **kwargs):
+	  """Sets up temporary flask context
 
-	Generator function.
+	  Generator function.
 
-	Yields:
-		A flask testing client attribute to Behave's context.
-	"""
-	# temporary db file code here
-	app.testing = True
-	context.client = app.test_client()
-	# with app.app_context():
-		# initialize db code here
-	yield context.client
+	  Yields:
+		  A flask testing client attribute to Behave's context.
+	  """
+	  # temporary db file code here
+	  app.testing = True
+	  context.client = app.test_client()
+	  context.request = app.test_request_context()
+	  # with app.app_context():
+		  # initialize db code here
+	  yield context.client
 
-	# close temp db code
-	# remove temp db code
-
-
+	  # close temp db code
+	  # remove temp db code
 
 def before_all( context ):
-	"""Runs before each test
+	  """Runs before each test
 
-	Args:
-		context:	Behave testing context
-	"""
-	use_fixture( splinter_browser, context )
-
+	  Args:
+  		  context:	Behave testing context
+	  """
+	  use_fixture(splinter_browser, context)
+	  use_fixture(flask_client, context)
 
 def before_tag( context, tag ):
-	"""Prepares fixtures based on tags
+	  """Prepares fixtures based on tags
 
-	Description
+	  Description
 
-	Args:
-		context:	Behave testing context
-		tag:		Tag passed via @fixture.[tag]
-	"""
-	if tag == "fixture.browser":
-		use_fixture( splinter_browser, context )
+	  Args:
+		    context:	Behave testing context
+		    tag:		Tag passed via @fixture.[tag]
+	  """
+	  if tag == "fixture.browser":
+		    use_fixture(splinter_browser, context)
+	  if tag == "fixture.client":
+	      use_fixture(flask_client, context)
--- features/steps/front_page.py
+++ features/steps/front_page.py
@@ -3,6 +3,6 @@ from flask import url_for
 
 @given('I am on the landing page')
 def step_impl(context):
-    #browser = Browser('flask', app=app )
-    context.browser.visit('http://localhost:5000/')
-    assert(context.browser.title == 'Home - Note Weaver')
+    with context.request:
+        context.browser.visit(url_for('index'))
+        assert(context.browser.url == url_for('index'))
