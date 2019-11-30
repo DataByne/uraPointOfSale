7f2b8948abc5452a8c1acfcb966a9c687d682647
Ryan Yoak <ryan.yoak@gmail.com>
2019-11-29 22:29:52 -0500

    first test passes

--- features/steps/front_page.py
+++ features/steps/front_page.py
@@ -1,11 +1,8 @@
 from behave import *
 from flask import url_for
-import environment
-from splinter import Browser
-from app import app
 
 @given('I am on the landing page')
 def step_impl(context):
     #browser = Browser('flask', app=app )
-    context.browser.get(url_for('index'))
-    assert(context.browser.url() == 'http://localhost:5000/')
+    context.browser.visit('http://localhost:5000/')
+    assert(context.browser.title == 'Home - Note Weaver')
