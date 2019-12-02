b988986f3b7031f573eeee46c6103a0ef35ae2ec
Kyle Pugh <kpugh1@kent.edu>
2019-12-02 17:36:55 -0500

    Allow specific styling in markup.

--- app/__init__.py
+++ app/__init__.py
@@ -73,7 +73,7 @@ def markup_text_filter(text):
         text,
         tags=['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'p', 'small', 'span', 'strong', 'ul'],
         attributes={'a': ['href', 'title'], 'abbr': ['title'], 'acronym': ['title'], '*': ['style']},
-        styles=['*'],
+        styles=['color', 'background-color', 'font-family', 'font-size', 'font-decoration'],
         protocols=['http', 'https', 'mailto']
     ))
 
